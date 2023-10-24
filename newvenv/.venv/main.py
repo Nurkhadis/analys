8cb99999178c7cf238b00a77cdea5adfc55e845c
Nurkhadis <nurkhadissandibek@gmail.com>
2023-10-24 19:05:39 +0600

    second

--- newvenv/.venv/main.py
+++ newvenv/.venv/main.py
@@ -1,3 +1,4 @@
+#%%
 import sqlite3
 import psycopg2
 import pandas as pd
@@ -11,23 +12,26 @@ connection = psycopg2.connect(
     port=5432,
 )
 
-#connection = sqlite3.connect("sample_database")
 
 if connection:
     print("connection is set...")
 else:
     print("connection is not set...")
 
-# cusor = connection.cursor()
-# query_car_list = """select "model", "mpg", "cyl", "disp" from "mtcars" """
-# cusor.execute(query_car_list)
-# car_list = cusor.fetchall()
-# print("model", " | ", "mpg", " | ", "cyl", " | ", "disp")
-# for row in car_list:
-#     print(row[0], " | ", row[1], " | ", row[2], " | ", row[3])
-
 
 query = "select * from mtcars"
 df = pd.read_sql_query(query, connection)
 
 print(df)
+# #%%
+# print(df.head(5))
+# # %%
+# df.tail(5)
+# # %%
+# df.dtypes
+# # %%
+# df = df.drop(['mpg', 'disp', 'wt', 'vs', 'am'], axis=1)
+# df.head(5)
+# # %%
+# df = df.drop(['mpg', 'disp', 'wt', 'vs', 'am'], axis=1)
+# df.head(5)
